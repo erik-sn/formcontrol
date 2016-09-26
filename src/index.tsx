6939c00d52dedb0916dec3b8e4eb35df8051288e
kiresuah <kiresuah@gmail.com>
2016-09-25 23:01:29 -0400

    Added material-ui

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,13 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 import { Router, browserHistory } from "react-router";
+import * as injectTapEventPlugin from "react-tap-event-plugin";
 import { applyMiddleware, compose, createStore } from "redux";
 import * as thunk from "redux-thunk";
+
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
 
 import reducers from "./reducers/root";
 import router  from "./routes";
